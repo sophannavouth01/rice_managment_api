9cfa8590a3d4463b5d5e28637510119bf9aee0b6
Sophanna <sophanna@Sophannas-MacBook-Pro.local>
2024-02-29 11:57:04 +0700

    ADD cmd for AFTER clone project on main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,14 @@ async function bootstrap() {
   
   app.enableCors();
   await app.listen(3000);
+
+  // after clone please create file .env and pass this code to file .env 
+
+  // DB_HOST=127.0.0.1
+  // DB_PORT=3306
+  // DB_USERNAME=root
+  // DB_PASSWORD=230202
+  // DB_NAME=  rice_managment_db
+   
 }
 bootstrap();
