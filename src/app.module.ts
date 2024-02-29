e4e2c7d3bb61f7f194bf45a2a55aa643e0f55328
Sophanna <sophanna@Sophannas-MacBook-Pro.local>
2024-02-29 11:40:48 +0700

    first commit connect db already

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { join } from 'path';
+
+@Module({
+  imports: [ ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [join(process.cwd(), 'dist/**/*.entity.js')],
+        // do NOT use synchronize: true in real projects
+        synchronize: true,
+      }),
+    }),
+    UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
