b06bc55d8dd5a2e59c45dd44dac3eba5a195d427
Sophanna <sophanna@Sophannas-MacBook-Pro.local>
2024-03-19 15:48:39 +0700

    update db change to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { join } from 'path';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
+        type: 'postgres',
         host: configService.get('DB_HOST'),
         port: +configService.get('DB_PORT'),
         username: configService.get('DB_USERNAME'),
